963e1889c7e67960fdb8e7d60bf68c3a51e959b9
Jianfei2333 <xjf999999@hotmail.com>
2019-05-25 15:38:36 +0800

    删除imagenet2012数据handler

--- DataUtils/imagenet2012.py
+++
@@ -1,8 +0,0 @@
-import os
-from torch.utils.data import DataLoader
-from torch.utils.data import sampler
-
-import torchvision.datasets as dset
-import torchvision.transforms as T
-
-DATAPATH = os.environ['datapath']
