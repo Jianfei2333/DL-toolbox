06c59220629a752aa5c0888d802a3bcedce5bbf4
Jianfei2333 <xjf999999@hotmail.com>
2019-05-31 17:48:07 +0800

    修改globalparser的bug

--- config/globalparser.py
+++ config/globalparser.py
@@ -6,7 +6,7 @@ def getparser():
   parser.add_argument('-b', '--batch-size', help='Assign batch size, default 64.', default='64')
   parser.add_argument('-c', '--continue', help='Continue training, default False.', default=False, action='store_true')
   parser.add_argument('-p', '--pretrain', help='Pretrain epochs.', default='0')
-  parser.add_argument('-e', '--epochs', help='Epochs to train, default 20.', default=20)
+  parser.add_argument('-e', '--epochs', type=int, help='Epochs to train, default 20.', default=20)
   parser.add_argument('--save-every', help='Save the model every n epochs, default 10.', default='10')
   parser.add_argument('--print-every', help='Print the model every n steps, default 10.', default='10')
   return parser
