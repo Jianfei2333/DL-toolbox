25c88b86e6c5573f821861530fe633a2e7f29c2c
Jianfei2333 <xjf999999@hotmail.com>
2019-06-11 20:45:49 +0800

    添加自定义loss

---
+++ Networks/Losses/RecallWeightedCrossEntropy.py
@@ -0,0 +1,19 @@
+import torch
+import torch.nn as nn
+
+class RecallWeightedCrossEntropy(nn.Module):
+  def __init__(self, weights):
+    self.weights = weights
+
+  def forward(self, scores, y):
+    _, pred = scores.max(1)
+    recall_weights = weights[pred]
+    exp_scores = torch.exp(scores)
+    softmax = exp_scores[:, pred].diagonal() / exp_scores.sum(1)
+    cross_entropy = -torch.log(softmax)
+    loss = torch.sum(recall_weights * cross_entropy)
+    return loss
+
+def recall_cross_entropy(scores, y, weight):
+  Res = RecallWeightedCrossEntropy(weight)
+  return Res(scores, y)
