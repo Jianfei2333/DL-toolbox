26ce53138f8ef7ac80c25e9c26162cde61c4cd4e
Jianfei2333 <xjf999999@hotmail.com>
2019-06-13 17:29:05 +0800

    修改数据集为颜色恒常后

--- config/globalconfig.py
+++ config/globalconfig.py
@@ -25,14 +25,14 @@ def run():
   os.environ['step'] = '0'
   if user.find('jianfei') == -1:
     os.environ['where_am_i'] = 'pc'
-    os.environ['datapath'] = '/home/huihui/Data/ISIC2018/'
+    os.environ['datapath'] = '/home/huihui/Data/ISIC2018_with_Color_Constancy/'
     os.environ['device'] = 'cpu'
     os.environ['tb-logdir'] = '/home/huihui/Log/tensorboard-log/'
     os.environ['logfile-dir'] = '/home/huihui/Log/runlog/'
     os.environ['savepath'] = '/home/huihui/Models/'
   else:
     os.environ['where_am_i'] = 'lab'
-    os.environ['datapath'] = '/data0/share/ISIC2018/'
+    os.environ['datapath'] = '/data0/share/ISIC2018_with_Color_Constancy/'
     os.environ['device'] = 'cuda:'+d.get_gpu_choice()
     os.environ['tb-logdir'] = '/data0/jianfei/tensorboard-log/'
     os.environ['logfile-dir'] = '/data0/jianfei/runlog/'
