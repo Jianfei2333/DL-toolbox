f26b00df016990589e3ed16b6c6f7b779b757950
Jianfei2333 <xjf999999@hotmail.com>
2019-06-24 20:11:37 +0800

    修改submitfile的逻辑

--- config/globalparser.py
+++ config/globalparser.py
@@ -8,7 +8,8 @@ def getparser():
   parser.add_argument('-e', '--epochs', type=int, help='Epochs to train, default 20.', default=20)
   parser.add_argument('--save-every', help='Save the model every n epochs, default 10.', default='10')
   parser.add_argument('--print-every', help='Print the model every n steps, default 100.', default='100')
-  parser.add_argument('--gpus', type=int, help='Choose the total number of gpus to use.', default=1)
   parser.add_argument('--data', help='Set the data path.')
   parser.add_argument('--model', help='Set the model path.')
+  parser.add_argument('--gpus', type=int, help='Choose the total number of gpus to use.', default=1)
+  parser.add_argument('--type', help='Type of submit file to generate, default "Val", choice ("Val", "Test")', default='Val')
   return parser
