e5ad04e9c2e0fa1095b6db2e4daca87bf9ef9f93
Jianfei2333 <xjf999999@hotmail.com>
2019-06-29 16:52:54 +0800

    放宽num_workers限制

--- DataUtils/ImgFolder_5fold.py
+++ DataUtils/ImgFolder_5fold.py
@@ -11,7 +11,7 @@ import torchvision.datasets as dset
 
 datapath = os.environ['datapath']
 
-def getdata(transform={'train':None, 'val':None}, kwargs={'num_workers': 4, 'pin_memory': True}):
+def getdata(transform={'train':None, 'val':None}, kwargs={'num_workers': 10, 'pin_memory': True}):
 
   print ("Collecting data ...")
 
