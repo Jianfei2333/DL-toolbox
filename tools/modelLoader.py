ab45834388f902c693ccdf53898b962dddf64ccb
Jianfei2333 <xjf999999@hotmail.com>
2019-06-29 15:53:07 +0800

    fix bug：model中的参数在parallel过程中的传递问题

--- tools/modelLoader.py
+++ tools/modelLoader.py
@@ -10,6 +10,10 @@ def load(model):
   else:
     ids = os.environ['device'][5:]
     ids = [int(x) for x in ids.split(',')]
+    step = model.step
+    epochs = model.epochs
     model = nn.DataParallel(model, device_ids=ids)
+    model.step = step
+    model.epochs = epochs
     model = model.to(device='cuda:{}'.format(ids[0]))
   return model
