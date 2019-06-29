c794a281f3f0744dc1a5de6239e4e73f3870ed7e
Jianfei2333 <xjf999999@hotmail.com>
2019-06-29 16:48:08 +0800

    添加累计batch

--- config/globalparser.py
+++ config/globalparser.py
@@ -4,6 +4,7 @@ def getparser():
   parser = argparse.ArgumentParser()
   parser.add_argument('-lr', '--learning-rate', type=float, help='Assign learning rate, default 1e-4.', default=1e-4)
   parser.add_argument('-b', '--batch-size', help='Assign batch size, default 64.', default='64')
+  parser.add_argument('--batch-scale', help='Assign batch scale, the model will update its params every k batches. The actual batch size will be k*b', default='1')
   parser.add_argument('-c', '--continue', help='Continue training, default False.', default=False, action='store_true')
   parser.add_argument('-e', '--epochs', type=int, help='Epochs to train, default 20.', default=20)
   parser.add_argument('--save-every', help='Save the model every n epochs, default 10.', default='10')
