030933fb133c7e5dd7fde3d3ec7d50d482cb3f3b
Jianfei2333 <xjf999999@hotmail.com>
2019-07-05 15:19:36 +0800

    添加Effb5doc入口

---
+++ Loader/Model/Efficientnetb5_DOC.py
@@ -0,0 +1,39 @@
+from efficientnet_pytorch import EfficientNet
+from tools import modelLoader
+import torch.nn as nn
+
+from config import globalconfig
+import os
+
+def load(info, Continue=False):
+  models = [None, None, None, None, None]
+  for i in range(5):
+    models[i] = EfficientNet.from_pretrained('efficientnet-b5')
+    # Modify.
+    num_fcin = models[i]._fc.in_features
+    models[i]._fc = nn.Linear(num_fcin, len(info['classes'])-2)
+
+  # print (model)
+
+  params = []
+  for i in range(5):
+    # models[i] = models[i].to(device='cpu')
+    models[i] = modelLoader.load(models[i])
+    params_to_update = []
+    for name,param in models[i].named_parameters():
+      if param.requires_grad == True:
+        params_to_update.append(param)
+    params.append(params_to_update)
+  
+  if Continue:
+    models = globalconfig.loadmodels(models)
+  else:
+    for i in range(5):
+      models[i].step=0
+      models[i].epochs=0
+  
+  modelinfo = {
+    'inputsize': (456, 456)
+  }
+
+  return (models, params, modelinfo)
