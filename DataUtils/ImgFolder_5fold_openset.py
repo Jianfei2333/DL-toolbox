a1abfe7583f9866814242abbc5f69e539c2899bf
Jianfei2333 <xjf999999@hotmail.com>
2019-07-05 14:04:05 +0800

    test run：添加DOC入口

---
+++ DataUtils/ImgFolder_5fold_openset.py
@@ -0,0 +1,87 @@
+import os
+import pandas as pd
+import numpy as np
+from PIL import Image
+from torch.utils.data import Dataset, DataLoader
+from torch.utils.data import sampler
+import torch
+import copy
+
+import torchvision.transforms as T
+import torchvision.datasets as dset
+
+datapath = os.environ['datapath']
+
+def getdata(unknown = [0, 3], transform={'train':None, 'val':None}, kwargs={'num_workers': 20, 'pin_memory': True}):
+
+  print ("Collecting data ...")
+
+  traindata = dset.ImageFolder(datapath+'Data', transform=transform['train'])
+  train4valdata = dset.ImageFolder(datapath+'Data', transform=transform['val'])
+  valdata = dset.ImageFolder(datapath+'Data', transform=transform['val'])
+  
+  t = 0
+  unknown_idxs = np.array([])
+  for img, label in traindata:
+    if label in unknown:
+      traindata.targets[t] = -1
+      train4valdata.targets[t] = -1
+      valdata.targets[t] = -1
+      unknown_idxs = np.hstack((unknown_idxs, t))
+    t += 1
+
+  c2i = copy.deepcopy(traindata.class_to_idx)
+  for k in traindata.class_to_idx:
+    if traindata.class_to_idx[k] in unknown:
+      del(c2i[k])
+  c2i['unknown'] = -1
+  traindata.class_to_idx = c2i
+  traindata.classes = list(c2i.keys())
+  train4valdata.class_to_idx = c2i
+  train4valdata.classes = list(c2i.keys())
+  valdata.class_to_idx = c2i
+  valdata.classes = list(c2i.keys())
+
+  labels = np.array(traindata.imgs)[:, 1]
+  C = len(traindata.classes)
+
+  # weights: The number of images in different classes
+  weights = np.zeros(C)
+  for i in range(C):
+    weights[i] = np.where(labels == str(i), 1, 0).sum()
+  traindata.weights = weights
+  valdata.weights = weights
+
+  batch = int(os.environ['batch-size'])
+
+  fold_ind = [np.array([]) for i in range(5)]
+  for _ in range(5):
+    fold_ind[_] = np.array(np.load(datapath+str(_)+'fold.npy'), dtype='int')
+
+  dataloader_5folds = [0,0,0,0,0]
+  for k in range(5):
+    VAL_IND = 4-k
+    train_ind = np.array([])
+    val_ind = np.array([])
+    for i in range(5):
+      if i == VAL_IND:
+        val_ind = fold_ind[i]
+      else:
+        train_ind = np.hstack((train_ind, fold_ind[i])).astype('int')
+
+    to_move_ind = np.intersect1d(train_ind, unknown_idxs)
+    train_ind = np.setdiff1d(train_ind, to_move_ind)
+    val_ind = np.union1d(val_ind, to_move_ind)
+
+    train_dataloader = DataLoader(traindata, batch_size=batch, sampler=sampler.SubsetRandomSampler(train_ind), **kwargs)
+    train4val_dataloader = DataLoader(train4valdata, batch_size=batch, sampler=sampler.SubsetRandomSampler(train_ind), **kwargs)
+    val_dataloader = DataLoader(valdata, batch_size=batch, sampler=sampler.SubsetRandomSampler(val_ind), **kwargs)
+    dataloader_5folds[k] = {
+      'train': train_dataloader,
+      'train4val': train4val_dataloader,
+      'val': val_dataloader
+    }
+
+  print ("Collect data complete!\n")
+
+  return dataloader_5folds
---
+++ main_DOC.py
@@ -0,0 +1,60 @@
+# Global environment setup.
+import os
+# Arg parser
+from config import globalparser
+args = vars(globalparser.getparser().parse_args())
+
+from config import globalconfig
+globalconfig.run(args)
+
+print ('Train {} with {}.(Running on {})'.format(os.environ['savepath'], os.environ['datapath'], os.environ['device']))
+
+import importlib
+# Essential network building blocks.
+model = importlib.import_module('Loader.Model.'+args['model'])
+transform = importlib.import_module('Loader.Transform.train_aug')
+
+# Data loader.
+from DataUtils import ImgFolder_5fold_openset as data
+
+# Official packages.
+import torch.nn as nn
+import torch.optim as optim
+
+# 下面开始进行主干内容
+from tools import datainfo
+info = datainfo.getdatainfo(os.environ['datapath'])
+
+models, params, modelinfo = model.load(info, args['continue'])
+
+# from tools import modelLoader
+# for i in range(len(models)):
+#   models[i] = modelLoader.load(models[i])
+
+transform = transform.load(modelinfo, info)
+
+# GOT DATA
+dataloaders = data.getdata(transform)
+
+# DEFINE MODEL
+
+# DEFINE OPTIMIZER
+optimizers = [None, None, None, None, None]
+for i in range(5):
+  optimizers[i] = optim.SGD(params[i], lr=args['learning_rate'], momentum=0.9)
+  # optimizer = optim.Adam(params[i], lr=args['learning_rate'])
+
+# criterion = nn.functional.cross_entropy
+# criterion = RecallWeightedCrossEntropy.recall_cross_entropy
+# if args['loss'] != '':
+criterion = importlib.import_module('Loader.Loss.DOC').loss
+
+from tools import train_and_check as mtool
+
+mtool.train5folds_DOC(
+  models,
+  dataloaders,
+  optimizers,
+  criterion,
+  args['epochs']
+)
